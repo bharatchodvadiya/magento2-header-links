b47e5f50adcbe02a57c3686f41d6e4585071ac00
Bharat Chodvadiya <bharat.chodvadiya@gmail.com>
2018-12-31 23:36:45 +0530

    Add files via upload

---
+++ app/code/Custom/Vendor/CustomLink.php
@@ -0,0 +1,15 @@
+<?php
+namespace Custom\Vendor\Block;
+
+class CustomLink extends \Magento\Framework\View\Element\Html\Link
+{
+    /**
+     * Render block HTML.
+     *
+     * @return string
+     */
+    protected function _toHtml()
+    {
+        return parent::_toHtml();
+    }
+}
